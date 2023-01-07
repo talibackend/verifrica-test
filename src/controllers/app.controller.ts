6d39dd1095f6a15e3b5e8c905216f6807e504874
Femi Fatokun <76601789+talibackend@users.noreply.github.com>
2023-01-07 16:07:58 +0100

    Understood request and response injection

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, Res } from '@nestjs/common';
 import { AppService } from '../services/app.service';
+import { Request, Response } from 'express';
 
 @Controller('/')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('hello')
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@Req() req: Request, @Res() res: Response): Response {
+    return this.appService.getHello(req, res);
   }
 
   @Get('hi')
--- src/services/app.service.ts
+++ src/services/app.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { Request, Response } from 'express';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(req : Request, res : Response): Response {
+    return res.status(200).json({ hello : "world..." });
   }
   getHi(): string{
     return 'Hi world...'
