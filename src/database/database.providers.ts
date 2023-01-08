90ae9d7f50beb0589786771b4f21f27c9d90a9ef
Femi Fatokun <76601789+talibackend@users.noreply.github.com>
2023-01-08 13:53:49 +0100

    Added Post Model

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -2,6 +2,7 @@ import { env } from 'process';
 import { Sequelize } from 'sequelize-typescript';
 import { User } from '../modules/user/user.entity';
 import { Follow } from '../modules/follow/follow.entity';
+import { Post } from '../modules/post/post.entity';
 
 export const databaseProviders = [
   {
@@ -15,7 +16,7 @@ export const databaseProviders = [
         password: env.PG_PASSWORD,
         database: env.PG_DB,
       });
-      sequelize.addModels([User, Follow]);
+      sequelize.addModels([User, Follow, Post]);
       await sequelize.sync({ alter : true });
       return sequelize;
     },
---
+++ src/modules/post/post.entity.ts
@@ -0,0 +1,18 @@
+import { Table, Model, Column, DataType } from 'sequelize-typescript';
+import { User } from '../user/user.entity';
+
+@Table
+
+export class Post extends Model {
+    @Column({ type : DataType.INTEGER, primaryKey : true, autoIncrement : true, allowNull : false })
+    id : number
+
+    @Column({ type : DataType.INTEGER, allowNull : false, references : { model : User, key : 'id' } })
+    user_id : number
+
+    @Column({ type : DataType.TEXT, allowNull : false })
+    content : string
+
+    @Column({ type : DataType.INTEGER, defaultValue : 0 })
+    views : number
+}
