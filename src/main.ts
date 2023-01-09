42475288e5197df8bf61094dd723bcddacbebefc
Femi Fatokun <76601789+talibackend@users.noreply.github.com>
2023-01-09 01:58:15 +0100

    Added some swags

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
-import * as dotenv from 'dotenv'
+import * as dotenv from 'dotenv';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API Docs')
+    .setDescription('The REST API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+
   await app.listen(3000);
 }
 bootstrap();
