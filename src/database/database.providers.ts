94fbbd986e02b5a63bf4174aad005daf0e6869d0
Femi Fatokun <76601789+talibackend@users.noreply.github.com>
2023-01-09 01:47:47 +0100

    Cleanup

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -10,12 +10,8 @@ export const databaseProviders = [
     provide: 'SEQUELIZE',
     useFactory: async () => {
       const sequelize = new Sequelize({
-        dialect: 'postgres',
-        host: env.PG_HOST,
-        port: parseInt(env.PG_PORT),
-        username: env.PG_USERNAME,
-        password: env.PG_PASSWORD,
-        database: env.PG_DB,
+        dialect: 'sqlite',
+        storage : 'db.sqlite'
       });
       sequelize.addModels([User, Follow, Post, Like]);
       await sequelize.sync({ alter : true });
