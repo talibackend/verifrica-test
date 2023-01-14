dcfd4e82685f3939db38e38601c6fa39265c9b2d
Femi Fatokun <76601789+talibackend@users.noreply.github.com>
2023-01-14 18:30:10 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ async function bootstrap() {
     .setTitle('API Docs')
     .setDescription('The REST API description')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
